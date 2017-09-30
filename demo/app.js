73ea52766ad679cdc58f125890191c30bb6ed2d8
0xFireball <0xFireball@outlook.com>
2017-09-29 22:12:51 -0700

    include chat names in demo

--- demo/app.js
+++ demo/app.js
@@ -9,7 +9,9 @@ const App = new Vue({
     peerConnected: false,
     connectedPeer: null,
     messages: [],
-    c_message: null
+    c_message: null,
+    chat_name: null,
+    peer_chat_name: null
   },
   methods: {
     connectClient: function () {
@@ -26,7 +28,7 @@ const App = new Vue({
     connectPeer: function () {
       let peerId = this.peer_id
       console.log(`connecting to ${peerId}`)
-      this.client.sendTo(peerId, '>>>').then(() => {
+      this.client.sendTo(peerId, '>>>' + this.chat_name).then(() => {
         // successfully connected
         this.peerConnected = true
         this.connectedPeer = peerId
@@ -36,23 +38,34 @@ const App = new Vue({
     dataReceived: function (d) {
       console.log('data: ', d.data)
       if (!this.peerConnected) {
-        if (d.data === '>>>') {
+        if (d.data.startsWith('>>>')) {
           // connect request
           console.log(`connection from peer ${d.source}`)
           // accept connection
+          this.peer_chat_name = d.data.substring(3)
           this.peerConnected = true
           this.connectedPeer = d.source
+          // send chat name
+          this.client.sendTo(this.connectedPeer, '<<<' + this.chat_name)
         }
+      } else if (d.data.startsWith('<<<')) {
+        this.peer_chat_name = d.data.substring(3)
       } else {
         // message received
-        this.messages.push(d.data)
+        this.messages.push({
+          m: d.data,
+          n: this.peer_chat_name
+        })
       }
     },
     sendMessage: function () {
       // send message
       let msg = this.c_message
       this.c_message = '' // clear message
-      this.messages.push(msg)
+      this.messages.push({
+        m: msg,
+        n: this.chat_name
+      })
       // send to peer
       this.client.sendTo(this.connectedPeer, msg)
     }
